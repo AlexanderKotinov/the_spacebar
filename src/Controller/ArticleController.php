c0d7f06a27560a50e6be9be8251423a267c80950
AlexanderKotinov <alexander777ck@gmail.com>
2018-10-17 22:58:30 +0300

    Added debug recipies.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -9,7 +10,7 @@ class ArticleController extends AbstractController
 {
     public function homepage()
     {
-        return new Response('Test msg!');
+        return $this->render('article/homepage.html.twig', []);
     }
 
     public function show($slug)
@@ -19,10 +20,18 @@ class ArticleController extends AbstractController
             'Second comment',
             'Third comment'
             ];
+        dump($slug, $this);
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
-            'comments' => $comments
+            'comments' => $comments,
+            'slug' => $slug
         ]);
+    }
+
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    {
+        $logger->info('Article is being liked!');
+        return $this->json(['hearts' => rand(5,20)]);
     }
 }
