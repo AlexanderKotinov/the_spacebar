f41bd6c431b3b8880709dc5837567696cf8dff1a
AlexanderKotinov <alexander777ck@gmail.com>
2018-10-22 17:53:14 +0300

    Added security

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -4,11 +4,24 @@ namespace App\Controller;
 
 use App\Repository\CommentRepository;
 use Knp\Component\Pager\PaginatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class CommentAdminController
+ * @package App\Controller
+ * @IsGranted("ROLE_ADMIN")
+ */
+
 class CommentAdminController extends AbstractController
 {
+    /**
+     * @param CommentRepository $repository
+     * @param Request $request
+     * @param PaginatorInterface $paginator
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function index(CommentRepository $repository, Request $request, PaginatorInterface $paginator)
     {
         $q = $request->query->get('q');
