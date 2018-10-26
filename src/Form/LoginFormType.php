8558816dee2bf01ed08067da1af5895f3da5342c
AlexanderKotinov <alexander777ck@gmail.com>
2018-10-26 20:36:11 +0300

    Added form style class.

--- src/Form/LoginFormType.php
+++ src/Form/LoginFormType.php
@@ -3,6 +3,9 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,8 +18,13 @@ class LoginFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('password')
+            ->add('email', EmailType::class, ['attr' => ['class' => 'form-control']])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Password', 'attr' => ['class' => 'form-control']],
+                'second_options' => ['label' => ' Confirm Password', 'attr' => ['class' => 'form-control']]
+
+            ])
             ->add('save', SubmitType::class)
         ;
     }
